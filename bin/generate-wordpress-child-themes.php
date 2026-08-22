36228f28362e32b610175f72de5bbcfa35e5e254
Dazzle Software, LLC <dazzle.software@gmail.com>
2026-08-22 13:10:30 -0600

    added wordpress child themes

---
+++ bin/generate-wordpress-child-themes.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Generates a minimal WordPress child theme for every Genesis parent theme.
+ */
+
+$themesDirectory = dirname(__DIR__) . '/themes';
+$templateFiles = $themesDirectory . '/helium-child/wordpress';
+
+foreach (new DirectoryIterator($themesDirectory) as $theme) {
+    if (!$theme->isDir() || $theme->isDot() || str_ends_with($theme->getFilename(), '-child')) {
+        continue;
+    }
+
+    $name = $theme->getFilename();
+    $parentDirectory = $theme->getPathname() . '/wordpress';
+    $childDirectory = $themesDirectory . "/{$name}-child/wordpress";
+
+    if (!is_dir($parentDirectory) || is_dir($childDirectory)) {
+        continue;
+    }
+
+    mkdir($childDirectory . '/genesis', 0777, true);
+    mkdir($childDirectory . '/includes', 0777, true);
+
+    $displayName = preg_replace_callback(
+        '/(^|[-_ ])([a-z])/',
+        static fn(array $match): string => $match[1] . strtoupper($match[2]),
+        $name
+    );
+
+    $style = file_get_contents($templateFiles . '/style.css');
+    $style = str_replace(
+        ['Helium Child', 'the Helium Genesis theme', 'genesis_helium'],
+        ["{$displayName} Child", "the {$displayName} Genesis theme", "genesis_{$name}"],
+        $style
+    );
+    file_put_contents($childDirectory . '/style.css', $style);
+
+    copy($templateFiles . '/functions.php', $childDirectory . '/functions.php');
+    copy($templateFiles . '/includes/theme.php', $childDirectory . '/includes/theme.php');
+
+    $yaml = file_get_contents($parentDirectory . '/genesis/theme.yaml');
+    $yaml = preg_replace('/^(\s*name:\s*).+$/m', '$1' . $displayName . ' Child', $yaml, 1);
+    $yaml = preg_replace('/^(\s*description:\s*).+$/m', '$1' . $displayName . ' Child Theme', $yaml, 1);
+    $yaml = preg_replace(
+        '/^(\s{2}theme:\s*\R)(?!\s{4}parent:)/m',
+        "$1    parent: genesis_{$name}\n",
+        $yaml,
+        1
+    );
+    file_put_contents($childDirectory . '/genesis/theme.yaml', $yaml);
+
+    foreach (['screenshot.png', 'screenshot.jpg', 'screenshot.jpeg'] as $screenshot) {
+        if (is_file($parentDirectory . '/' . $screenshot)) {
+            copy($parentDirectory . '/' . $screenshot, $childDirectory . '/' . $screenshot);
+            break;
+        }
+    }
+
+    echo "Generated {$name}-child\n";
+}
---
+++ themes/ambrosia-child/wordpress/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package   Genesis
+ * @author    Dazzle Software https://dazzlesoftware.org
+ * @copyright Copyright (C) 2026 Dazzle Software, LLC
+ * @license   GNU/GPLv3 and later
+ */
+
+defined('ABSPATH') or die;
+
+\add_action('wp_enqueue_scripts', static function() {
+    \wp_enqueue_style('parent-style', \get_template_directory_uri() . '/style.css');
+});
